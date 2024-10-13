873a8146fd50887d8daad3c84d76ea101c4b1704
9087000 <50361103+9087000@users.noreply.github.com>
2024-10-13 17:32:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+window onLoad = function(){
+ if(localStorage.getItem("username") && localStorage.getItem("password")){
+	 document.getElementById("existing").style.display = "block";
+ }
+
+ document.getElementById("existing").onClick = function() {
+	 let savedUSerName = localStorage.getItem("username");
+	 alert ("logged in as ${savedUsername}.");
+ }	
+
+ document.getElementById("loginForm").onSubmit = function(e) {
+	 e.preventDefault();
+
+	 let userName = document.getElementById("username").value;
+	 let password = document.getElementById("password").value;
+     let rememberMe = document.getElementById("rememberMe").checked;
+
+	 if(rememberMe) {
+		 localStorage.setItem("username", username);
+		 localStorage.setItem("password", password);
+	 } else {
+		 localStorage.removeItem("username");
+		 localStorage.removeITem("password");
+	 }
+
+	  alert(`Logged in as ${username}.`);
+ };	
+	
+};
